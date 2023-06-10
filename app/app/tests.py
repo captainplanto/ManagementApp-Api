fec2ddc6c85f394d36b9a1acc4d774ee7a675baa
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2023-06-10 16:55:44 +0800

    database setup and flake8 linting setting

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+# sample test
+
+from django.test import SimpleTestCase
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
+    def test_subt_numbers(self):
+        res = calc.subtract(12, 5)
+        self.assertEqual(res, 7)
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,19 @@
+# Django command to wait for database to start..
+# from django.db.utils import OperationalError
+from django.core.management.base import BaseCommand
+import time
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("waiting for database to connect....")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except ():
+                self.stdout.write(
+                    "Database unavailbale, waiting 1 second.....")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("database available!"))
